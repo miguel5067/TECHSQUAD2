b53cf97b121b3eef6801ca5177519499e07a9e81
miguel5067 <128329196+miguel5067@users.noreply.github.com>
2024-06-21 04:02:00 +0200

    Add files via upload

--- src/circuito1Prueba.ino
+++ src/circuito1Prueba.ino
@@ -4,52 +4,77 @@
 #include <SoftwareSerial.h>
 
 // Pines del módulo L298N
-int ENA = 5;  
-int IN1 = 4;
-int IN2 = 6;  
+int ENA = 7;  
+int IN1 = 6;
+int IN2 = 5;  
 
 Servo servo;  // para controlar el servomotor
-int velocidad = 200;  // Velocidad fija para el motor (regular!)
+int velocidad = 140;  // Velocidad fija para el motor
+int velocidadRetroceso = 220;  // Velocidad máxima para retroceder
 
 // Pines del sensor ultrasónico delantero
-int TRIG1 = 7;
-int ECHO1 = 8;
+int TRIG1 = 8;
+int ECHO1 = 9;
 
 // Pines del sensor ultrasónico izquierdo
-int TRIG2 = 10;
-int ECHO2 = 11;
+int TRIG2 = 3;
+int ECHO2 = 4;
+
+// pin del servo delantero
+int servoPin = 24;
 
 // COMANDOS DE DIRECCION 
 
 void moveForward() { // Avanzar hacia adelante
+  servo.write(90);  // Vuelve el servomotor a la posición inicial
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  servo.write(91);  // Vuelve el servomotor a la posición inicial
-}
-
-void Stop() { // Detenerse (si quieren lo pueden quitar)
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  servo.write(91);  // Vuelve el servomotor a la posición inicial
+  analogWrite(ENA, velocidad);  // Controla la velocidad del motor con el valor fijo establecido
 }
 
 void turnRight() { // Giro hacia la derecha 
-  servo.write(50); // (Regular el valor de giro)
+  servo.write(127); // (Regular el valor de giro)
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);  
+  analogWrite(ENA, velocidad);  // Controla la velocidad del motor con el valor fijo establecido
 }
 
 void turnLeft() { // Giro hacia la izquierda
-  servo.write(140); // (regular el valor de giro)
+  servo.write(35); // (Regular el valor de giro)
   digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);   
+  digitalWrite(IN2, LOW); 
+  analogWrite(ENA, velocidad);  // Controla la velocidad del motor con el valor fijo establecido
+}
+
+void moveBack() { // Retroceder
+  servo.write(127); // Vuelve el servomotor a la posición inicial
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, velocidadRetroceso);  // Aumenta la velocidad del motor para retroceder
+}
+
+void stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 0);  // Detiene el motor
+}
+
+float getDistance(int TRIG, int ECHO) {
+  pinMode(TRIG, OUTPUT);
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  pinMode(ECHO, INPUT);
+  return pulseIn(ECHO, HIGH, 30000) / 59.0;
 }
 
 // INICIALIZACION DE LOS PINES 
 
 void setup() {
   Serial.begin(9600);  // Inicia la comunicación serie
-  servo.attach(9); // Conecta el servomotor al pin 9
+  servo.attach(24); // Conecta el servomotor al pin 24
 
   // Configura los pines del módulo L298N como salidas
   pinMode(ENA, OUTPUT);
@@ -77,26 +102,27 @@ void loop() {
   Serial.print("Distancia Izquierda: ");
   Serial.println(distanciaIzquierda);
 
-  if (distanciaDelantera <= 15 && distanciaIzquierda <= 15) {  // Si ambos sensores detectan un obstáculo a 15 cm o menos
-  //Pueden variar esto 
-    Stop();
-    delay(100);
+  if (distanciaDelantera <= 15) {  // Si el sensor delantero detecta un obstáculo a 15 cm o menos
+    stop();
+    delay(2000); // Espera 2 segundos
+    moveBack();
+    delay(1750); // Retrocede por 4 segundos
+    stop(); // Detiene después de retroceder
+  } else if (distanciaDelantera <= 35 && distanciaIzquierda <= 35) {  // Si ambos sensores detectan un obstáculo a 35 cm o menos
     turnRight();
-    delay(580);
-    moveForward();
-    delay(450);
-  } else if (distanciaDelantera <= 15) {  // Si solo el sensor delantero detecta un obstáculo a 15 cm o menos
-  //Pueden variar esto 
-    Stop();
-    delay(100);
-    turnRight();
-    delay(580);
-    moveForward();
-    delay(450);
+    delay(1000); // Espera 1 segundo para girar
+  } else if (distanciaDelantera <= 35) {  // Si solo el sensor delantero detecta un obstáculo a 35 cm o menos
+    turnLeft();
+    delay(1000);  // Da un pequeño retraso para realizar el giro
+    distanciaDelantera = getDistance(TRIG1, ECHO1);  // Vuelve a medir la distancia después del giro
+    if (distanciaDelantera <= 15) {  // Si después del giro detecta un obstáculo a 15 cm o menos
+      stop();
+      delay(2000); // Espera 2 segundos
+      moveBack();
+      delay(1750); // Retrocede por 4 segundos
+      stop(); // Detiene después de retroceder
+    }
   } else {  // Si no hay obstáculo
     moveForward();
   }
-
-  analogWrite(ENA, velocidad);  // Controla la velocidad del motor con el valor fijo establecido
-
 }
